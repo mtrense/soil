800640ac6af7e8a178b54a0c7a747076d2717656
Max Trense <dev@trense.info>
2021-01-07 17:04:31 +0100

    Shortcut default functions into root package

--- logging/logging.go
+++ logging/logging.go
@@ -1,4 +1,4 @@
-package soil
+package logging
 
 import (
 	"io"
--- soil.go
+++ soil.go
@@ -1,1 +1,15 @@
 package soil
+
+import (
+	"github.com/mtrense/soil/config"
+	"github.com/mtrense/soil/logging"
+	"github.com/spf13/cobra"
+)
+
+func DefaultCLI(app *cobra.Command, version, commit, envPrefix string) {
+	config.DefaultCLI(app, version, commit, envPrefix)
+}
+
+func ConfigureDefaultLogging() {
+	logging.ConfigureDefaultLogging()
+}
